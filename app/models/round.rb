cbf4169889a25e15ed40b7443b0f0f098efbdac7
BarkleyRhoat <barkleyrhoat@gmail.com>
2026-07-27 13:08:47 -0400

    fixed syntax in round.rb

--- app/models/round.rb
+++ app/models/round.rb
@@ -2,7 +2,7 @@ class Round < ActiveRecord::Base
   belongs_to :player
   belongs_to :course
 
-validates :score, numericality: { greater_than_or_equal_to: 9 }
+  validates :score, numericality: { greater_than_or_equal_to: 9 }
   validates :player, presence: true
   validates :course, presence: true
 end
